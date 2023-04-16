f4a09675a91a8a2348ed9072a75931436bfe2283
Misnag <108684626+Misnag@users.noreply.github.com>
2023-04-17 06:51:06 +0900

    Delete cui directory

--- cui/portfolio1/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.config.from_object('portfolio1.config')
-
-from .main import User
-
-import portfolio1.main
--- cui/server.py
+++
@@ -1,4 +0,0 @@
-from portfolio1 import app
-
-if __name__ == '__main__':
-    app.run()
